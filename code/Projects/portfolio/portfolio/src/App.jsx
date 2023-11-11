4fb65deaabb041ac64f2858ca2a18a2654accfcf
TannerM03 <tanner.macpherson3@gmail.com>
2023-11-11 01:07:16 -0500

    first commit

---
+++ code/Projects/portfolio/portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+
+import stles from './App.module.css';
+
+function App() {
+
+  return (
+    <div className = {StyleSheet.App}>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ code/Projects/portfolio/portfolio/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <nav className = {StyleSheet.navbar}>
+        <a href = "/">Portfolio</a>
+        <div>
+            <ul>
+                <li><a href="#about"About></a></li>
+                <li><a href="#experience"Experience></a></li>
+                <li><a href="#projects"Projects></a></li>
+                <li><a href="#contact"Contact></a></li>
+
+            </ul>
+        </div>
+    </nav>
+  )
+}
